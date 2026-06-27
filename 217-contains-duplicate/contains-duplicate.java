80cf43b9a901c58770e3288fd9965fdcfe93245a
Tanmay Shinde <prime290604@gmail.com>
2026-06-27 20:46:58 +0530

    Time: 19 ms (32.13%) | Memory: 110.8 MB (6.09%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,15 +1,17 @@
 import java.util.Arrays;
 
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
-                return true;
-            }
+    public boolean containsDuplicate(int[] arr) {
+    HashMap<Integer , Integer> map = new HashMap<>();
+       for ( int i = 0 ; i < arr.length ; i++) {
+        if (map.containsKey(arr[i])) {
+            return true;
         }
+        else {
+            map.put(arr[i],1);
+        }
+       }
+       return false;
         
-        return false;
+        }
     }
-}
