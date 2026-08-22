22b4b2ff03a243a2434ea0cab4049ede90f8e4d6
Tanmay Shinde <prime290604@gmail.com>
2026-08-22 21:35:20 +0530

    Time: 1224 ms (5.27%) | Memory: 42.1 MB (58.32%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if (num == 1) return true;
+         for (int i = num / 2; i > 0; i--) {
+            if (i * i == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
